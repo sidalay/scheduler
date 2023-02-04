b81b096732180d1ebfe29e0c8109a33caec571c8
Sid Shingaki <sidalaytv@gmail.com>
2023-02-03 19:53:29 -0800

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import './style.scss';
+
+const Layout = () => {
+  return (
+    <>
+    <Header/>
+    <Outlet/>
+    <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path:"/",
+        element: <Home/>
+      },
+    ]
+  }
+]);
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
